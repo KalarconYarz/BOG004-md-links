d8e6ddf3ac99ec1c4679e53ce5503a611ebca38b
Karen Alarcon <akareny.10@gmail.com>
2022-04-26 22:06:10 -0500

    Se crea funcion que recorre el directorio y valida ruta archivo.md

--- src/md-links.js
+++ src/md-links.js
@@ -1,44 +1,91 @@
-const mdLinks = (args) => {
-  console.log("llego a md", args);
-  // Modulos de NODE
-  const fs = require("fs"); // file system modulo utilizado de node
-  const path = require("path"); //captura la ruta
+// Se importan los modulos de node-methods
+const {
+  converterPath,
+  validatePath,
+  isFileOrDirectory,
+  readDirectoryFiles,
+  isFileMd,
+  readFile,
+} = require('./node-methods')
+
+
+// se crea funcion mdlinks
+const mdLinks = (args) => new Promise ((resolve, reject) => {
+   //node methods filesystem - path
+   const fs = require('fs');
+   const path = require('path');
 
   // captura de ruta a partir de arry de args
-  const terminalPath = args[2];
+  const catchedPath = args[2];
+  console.log(catchedPath);
+  console.log('Es una ruta absoluta?', path.isAbsolute(catchedPath))
 
-  // Resuelve y normaliza la  ruta de relativa a absoluta
-  const pathAbsolute = path.resolve(terminalPath).normalize();
-  console.log("Ruta convertida Absoluta", pathAbsolute);
+  // Convierte ruta relativa a absoluta
+  const absolutePath = converterPath(catchedPath);
+  console.log('validando Path,', absolutePath);
 
-  // verifica si existe la ruta
-  const validatePath = (path) => fs.existsSync(path);
+  // Se Guarda el resultado de la ruta convertida invocando callback absolutePath
+  const resultValidatePath = validatePath(absolutePath);
+  console.log('Es una ruta Valida?,', resultValidatePath);
 
-  // función para saber si es un directorio o archivo si es directorio : true y si es archivo: false
-  const isFileOrDirectory = (pathToCheck) => {
-    fs.stat(pathToCheck, (err, stats) => {
-      if (err) throw err;
-      console.log("soy directorio?", stats.isDirectory());
-    });
-  };
-  // función para leer el contenido de mi archivo
-  const readFile = (pathToRead) => {
-    fs.readFile(pathToRead, "utf8", function (err, data) {
-      if (err) throw err;
-      console.log(data);
-    });
-  };
+// Se crea array para guardar el contenido de archivos.md
+const pathArray = [];
+//Condicional que valida la ruta
+    if(resultValidatePath){ // Ingresa sólo si es ruta válida
+      isFileOrDirectory(absolutePath)//ingresa sólo si es directorio
+        .then((isDirResult) => {
+            if(isDirResult){
+                console.log('OJO Recursividad');
+                const dirFiles = readDirectoryFiles(absolutePath);
+                resolve(dirFiles);
+                //Debería retornar un array con una o más rutas
+            }else{
+                console.log('Guardar la ruta md en array');
+                const isFileMdResult = isFileMd(absolutePath);
+                pathArray.push(isFileMdResult);
+                console.log(pathArray)
+                const fileContent = readFile(absolutePath); // debe leer el contenido del Archivo .md
+                resolve(fileContent);
+            }
+        })
+        .catch((error) => {
+            console.log('Archivo md no encontrado', error);
+        });
+    }else{
+        const invalidPath = 'Ruta Invalida';
+        console.log(invalidPath);
+        return invalidPath;
+    }    
+});
+
+
+//   // Se crea condicional si la ruta es valida
+//   if(resultValidatePath){ // Ingresa si la ruta es Valida
+//     console.log('Que Soy?', isFileOrDirectory(absolutePath));
+//     isFileOrDirectory(absolutePath) // ingresa si es un directorio
+//     .then((isDirResult) => {
+//       if (isDirResult) {
+//         console.log('Recusrividad Ojoo');
+//         const dirFiles = readDirFiles(absolutePath);
+//         resolve(promise.all(dirFiles));
+//       } else {
+//         const fileContent = readFile(absolutePath); // se debe revisar si es archivo.md
+//         console.log('Contenido del arhivo', fileContent);
+//         resolve(fileContent);
+//       }
+//     })
+//     .catch((error) => {
+//       console.log('Erroorr', error);
+//     });
+//   } else {
+//     console.log('OTRO else');
+//     const invalidPath = 'Ruta no Valida';
+//     reject(new Error(invalidPath));
+//   }
+// }
+
   // Guardo el rersultado e invoco la función pasando conmo argumento pathAbsolute
-  const resultValidatePath = validatePath(pathAbsolute);
-
-  if (resultValidatePath === true) {
-    isFileOrDirectory(pathAbsolute);
-    readFile(pathAbsolute);
-  } else {
-    console.log("Fin del programa");
-  }
-};
-
+  
 // Extrae el nombre de la extension del archivo
 // const extension = path.extname('prueba.md');
 // console.log('es un archivo md' + extension);
@@ -49,4 +96,6 @@ const mdLinks = (args) => {
 
 // });
 
+
+// se exporta el modulo para usar funcion mdLinks
 module.exports = mdLinks;
