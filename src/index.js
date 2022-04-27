58af2010270f56078e40306b572f4da16f310817
Karen Alarcon <akareny.10@gmail.com>
2022-04-26 22:04:59 -0500

    se comentan lineas de archivo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-// Se importa la funcion mdlinks
+// Se importa modulo para traer la funcion mdlinks
 const mdlinks = require('./md-links.js');
 
 // node methods process
 const process = require('process');
+
+// captura argumentos de la terminal
 const arguments = process.argv;
 
 // se invoca la funcion mdlinks
